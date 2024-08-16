8fcdfd6022eac665d5ddcadf6f5c459f47658e38
Leander Jedamus <ljedamus@web.de>
2024-08-16 12:24:29 +0200

    removed some obsolete files

--- errno.d
+++
@@ -1,2 +0,0 @@
-errno.o errno.d: Makefile Makefile.c_and_c++ errno.c project.h
-# automatically created 16.08.2024 09:32 by "make depend"
--- main.d
+++
@@ -1,2 +0,0 @@
-main.o main.d: Makefile Makefile.c_and_c++ main.c types.h getlocaledir.h gettext.h project.h version.h
-# automatically created 16.08.2024 09:32 by "make depend"
